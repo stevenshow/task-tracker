89a308712741f4ab5cda9c21c77cf96420de97fd
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-01-19 11:53:53 -0700

    take title to place at top of list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,9 @@
 import Card from './Card';
 
-export default function List({ items }) {
+export default function List({ items, title }) {
   return (
     <div className="border-1 flex w-60 flex-col gap-4 rounded border p-4">
+      <span>{title}</span>
       {items.map((item) => {
         return <Card title={item.title} key={item.id} />;
       })}
